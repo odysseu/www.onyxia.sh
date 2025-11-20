d87f0b69b1e906360c17be6ac3c307f598b2be0d
odysseu <uboucherie1@gmail.com>
2025-11-20 09:16:55 +0000

    Corrected the link thats now sends to the github project of this website instead of the github project of Onyxia

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 
-export const GITHUB_REPO_URL = "https://github.com/InseeFrLab/onyxia";
+export const GITHUB_REPO_URL = "https://github.com/InseeFrLab/www.onyxia.sh";
 export const DOCUMENTATION_URL = "https://docs.onyxia.sh";
